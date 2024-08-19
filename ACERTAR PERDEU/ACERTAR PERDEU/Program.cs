629ca14b48fa609449408e709605ad4637745afa
igolima16 <167836742+igolima16@users.noreply.github.com>
2024-08-19 18:07:54 -0300

    Add files via upload

---
+++ ACERTAR PERDEU/ACERTAR PERDEU/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACERTAR_PERDEU
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bem-vindo ao Jogo do Acertou: Perdeu! - Versão 2.0");
+
+
+            Random random = new Random();
+            int numeroOculto = random.Next(1, 101);
+
+
+            Console.Write("Digite a quantidade de jogadores: ");
+            int quantidadeJogadores = int.Parse(Console.ReadLine());
+
+            bool acertou = false;
+            int jogadorAtual = 1;
+
+            while (!acertou)
+            {
+                Console.Write($"Jogador {jogadorAtual}, digite seu palpite: ");
+                int palpite = int.Parse(Console.ReadLine());
+
+                if (palpite == numeroOculto)
+                {
+                    Console.WriteLine($"Acertou! Jogador {jogadorAtual} perdeu!");
+                    acertou = true;
+                }
+                else
+                {
+                    Console.WriteLine("Errado! Tente novamente.");
+                    jogadorAtual = jogadorAtual % quantidadeJogadores + 1;
+                }
+            }
+        }   }
+}
---
+++ ACERTAR PERDEU/ACERTAR PERDEU/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("ACERTAR PERDEU")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACERTAR PERDEU")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("d27cad3d-34e7-41f4-a5fe-e5c69e5d4008")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
